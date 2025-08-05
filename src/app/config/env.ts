135df2c2133bbc0c2370da1f8900bcd647788b2d
ratul0407 <rajaulislamratul12@gmail.com>
2025-08-05 23:12:23 +0600

    created multer and cloudinary config

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -27,6 +27,9 @@ interface envConfig {
   SSL_SUCCESS_FRONTEND_URL: string;
   SSL_FAIL_FRONTEND_URL: string;
   SSL_CANCEL_FRONTEND_URL: string;
+  CLOUDINARY_CLOUD_NAME: string;
+  CLOUDINARY_API_KEY: string;
+  CLOUDINARY_API_SECRET: string;
 }
 
 const loadEnvVariables = (): envConfig => {
@@ -56,6 +59,9 @@ const loadEnvVariables = (): envConfig => {
     "SSL_SUCCESS_FRONTEND_URL",
     "SSL_FAIL_FRONTEND_URL",
     "SSL_CANCEL_FRONTEND_URL",
+    "CLOUDINARY_CLOUD_NAME",
+    "CLOUDINARY_API_KEY",
+    "CLOUDINARY_API_SECRET",
   ];
   requiredEnvVariables.forEach((key) => {
     if (!process.env[key]) {
@@ -88,6 +94,9 @@ const loadEnvVariables = (): envConfig => {
     SSL_SUCCESS_FRONTEND_URL: process.env.SSL_SUCCESS_FRONTEND_URL as string,
     SSL_FAIL_FRONTEND_URL: process.env.SSL_FAIL_FRONTEND_URL as string,
     SSL_CANCEL_FRONTEND_URL: process.env.SSL_CANCEL_FRONTEND_URL as string,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
   };
 };
 export const envVars = loadEnvVariables();
