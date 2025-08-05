cf5c70700a2a6870930f5776825e4d7884d2e96a
ratul0407 <rajaulislamratul12@gmail.com>
2025-08-05 23:31:47 +0600

    set up cloudinary to upload imagaes

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 app.use("/api/v1", router);
