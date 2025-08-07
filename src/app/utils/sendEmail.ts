aa5105a259985c4c592e4fc6033862c6eb6f1197
ratul0407 <rajaulislamratul12@gmail.com>
2025-08-08 00:59:03 +0600

    created sendemail util function

---
+++ src/app/utils/sendEmail.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import nodeMailer from "nodemailer";
+import { envVars } from "../config/env";
+import path from "node:path";
+import ejs from "ejs";
+import AppError from "../errorHelpers/appError";
+const transporter = nodeMailer.createTransport({
+  secure: true,
+  auth: {
+    user: envVars.SMTP_USER,
+    pass: envVars.SMTP_PASS,
+  },
+  host: envVars.SMTP_HOST,
+  port: Number(envVars.SMTP_PORT),
+});
+
+interface sendEmailOptions {
+  to: string;
+  subject: string;
+  templateData?: Record<string, any>;
+  templateName?: string;
+  attachments?: [
+    { filename: string; content: Buffer | string; contentType: string }
+  ];
+}
+export const sendEmail = async ({
+  to,
+  templateData,
+  subject,
+  attachments,
+  templateName,
+}: sendEmailOptions) => {
+  try {
+    const templatePath = path.join(__dirname, `template/${templateName}.ejs`);
+    const html = await ejs.renderFile(templatePath, templateData);
+    const info = await transporter.sendMail({
+      from: envVars.SMTP_FROM,
+      to: to,
+      subject: subject,
+      html: html,
+      attachments: attachments?.map((attachment) => ({
+        filename: attachment.filename,
+        content: attachment.content,
+        contentType: attachment.contentType,
+      })),
+    });
+    console.log(`\u2709\uFE0F Email sent to ${to}: ${info.messageId}`);
+  } catch (error: any) {
+    console.log(`Email sending error ${error.message}`);
+    throw new AppError(401, "Email error");
+  }
+};
