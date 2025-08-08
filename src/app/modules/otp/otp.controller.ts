ad358e13802a9c9e50355e6cc019e2c1131535f2
ratul0407 <rajaulislamratul12@gmail.com>
2025-08-08 20:30:41 +0600

    completed otp routes with redis

--- src/app/modules/otp/otp.controller.ts
+++ src/app/modules/otp/otp.controller.ts
@@ -2,15 +2,30 @@
 import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { OTPService } from "./otp.service";
+import { sendResponse } from "../../utils/sendResponse";
 
 const sendOTP = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const result = await OTPService.sendOTP();
+    const { email, name } = req.body;
+    await OTPService.sendOTP(email, name);
+    sendResponse(res, {
+      success: true,
+      statusCode: 201,
+      message: "OTP sent successfully!",
+      data: null,
+    });
   }
 );
 const verifyOTP = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const result = await OTPService.verifyOTP();
+    const { email, otp } = req.body;
+    await OTPService.verifyOTP(email, otp);
+    sendResponse(res, {
+      success: true,
+      statusCode: 201,
+      message: "OTP verified successfully!",
+      data: null,
+    });
   }
 );
 export const OTPController = {
--- src/app/modules/otp/otp.service.ts
+++ src/app/modules/otp/otp.service.ts
@@ -1,6 +1,61 @@
-const sendOTP = async () => {};
+import crypto from "crypto";
+import { redisClient } from "../../config/redis.config";
+import { sendEmail } from "../../utils/sendEmail";
+import AppError from "../../errorHelpers/appError";
+import { User } from "../user/user.model";
 
-const verifyOTP = async () => {};
+const OTP_EXPIRATION = 2 * 60;
+const generateOTP = (length = 6) => {
+  const otp = crypto.randomInt(10 ** (length - 1), 1000000).toString();
+  return otp;
+};
+const sendOTP = async (email: string, name: string) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new AppError(404, "User not found");
+  }
+  if (user.isVerified) {
+    throw new AppError(401, "User is already verified");
+  }
+  const otp = generateOTP();
+
+  const redisKey = `otp:${email}`;
+  await redisClient.set(redisKey, otp, {
+    expiration: {
+      type: "EX",
+      value: OTP_EXPIRATION,
+    },
+  });
+  await sendEmail({
+    to: email,
+    subject: "Your OTP Code",
+    templateName: "otp",
+    templateData: {
+      name: name,
+      otp: otp,
+    },
+  });
+};
+
+const verifyOTP = async (email: string, otp: string) => {
+  const user = User.findOne({ email });
+  if (!user) {
+    throw new AppError(404, "User is not found");
+  }
+  const redisKey = `otp:${email}`;
+  const savedOtp = await redisClient.get(redisKey);
+  if (!savedOtp) {
+    throw new AppError(401, "Invalid OTP");
+  }
+  if (savedOtp !== otp) {
+    throw new AppError(401, "Invalid OTP");
+  }
+
+  await Promise.all([
+    User.updateOne({ email }, { isVerified: true }, { runValidators: true }),
+    redisClient.del([redisKey]),
+  ]);
+};
 export const OTPService = {
   sendOTP,
   verifyOTP,
