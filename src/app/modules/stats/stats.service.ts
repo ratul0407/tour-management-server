894258cadd29243cf5c1b1411a4200f0d1726b88
ratul0407 <rajaulislamratul12@gmail.com>
2025-08-09 22:06:12 +0600

    working on stats routes

---
+++ src/app/modules/stats/stats.service.ts
@@ -0,0 +1,19 @@
+const getUserStats = async () => {
+  return;
+};
+
+const getBookingStats = async () => {
+  return;
+};
+const getPaymentStats = async () => {
+  return;
+};
+const getTourStats = async () => {
+  return;
+};
+export const StatsService = {
+  getUserStats,
+  getBookingStats,
+  getPaymentStats,
+  getTourStats,
+};
